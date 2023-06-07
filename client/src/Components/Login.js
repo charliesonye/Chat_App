761cbf15c0ba3c32ca568f402bb69dca84ce5ed3
Charles <c_onyemem@live.com>
2023-06-06 23:49:25 -0700

    installed bootstrap and currently building out login component functionality

---
+++ client/src/Components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Form } from 'react-bootstrap'
+
+export default function Login() {
+  return (
+   <Container>
+    <Form>
+        <Form.Group>
+            <Form.Label>Enter in ID</Form.Label>
+            <Form.Control></Form.Control>
+        </Form.Group>
+    </Form>
+    Login
+   </Container>
+  )
+}
