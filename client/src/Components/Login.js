b08ddd063ce038a7c7d518bb98a2a3e2960c1429
Charles <c_onyemem@live.com>
2023-06-10 14:36:36 -0700

    Added uuid library to help create new when button prompt is clicked

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -1,5 +1,6 @@
 import React, {useRef} from 'react'
 import { Container, Form, Button } from 'react-bootstrap'
+import { v4 as uuidV4} from 'uuid'
 
 export default function Login({onIdSubmit}) {
 
@@ -9,6 +10,10 @@ export default function Login({onIdSubmit}) {
     e.preventDefault()
     onIdSubmit(idRef.current.value)
   }
+
+  function createNewId(){
+    onIdSubmit(uuidV4)
+  }
   return (
    <Container>
     <Form onSubmit= {handleSubmit}>
@@ -17,7 +22,7 @@ export default function Login({onIdSubmit}) {
             <Form.Control type= 'text' ref= {idRef} required/>
         </Form.Group>
         <Button type= 'submit' className= 'mr-2'>Login</Button>
-        <Button variant= 'secondary'>Create a New ID</Button>
+        <Button onClick={createNewId} variant= 'secondary'>Create a New ID</Button>
     </Form>
     Login
    </Container>
